825d1c940b7f88d61e0a938e0c85583aa58a42ea
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-17 15:26:16 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 import Popup from 'reactjs-popup'
 import Footer from '../layouts/footer';
 import ReactPlayer from 'react-player'
+import APIrouter from './api/sangkhlaAPI';
 function RightArrow(props) {
   const { className, style, onClick } = props;
   return (
@@ -123,7 +124,7 @@ export default function index() {
       console.log('boat house is ',boatHouses);
   }
   const getAccommodation =async()=>{
-    let accommodation_data = await axios.get('http://localhost:8080/get/accommodation')
+    let accommodation_data = await axios.get(`${APIrouter}/get/accommodation`)
     let fetch_accommodation = accommodation_data.data.payload
     for (let i = 0; i < fetch_accommodation.length; i++) {
       fetch_accommodation[i].type === "แพพัก" ? boatHouses.push(fetch_accommodation[i]) 
@@ -132,23 +133,23 @@ export default function index() {
     console.log(accommodation_data.data.payload);
   }
   const getBoatProvider = async()=>{
-    let boat_data = await axios.get('http://localhost:8080/get/boat-provider')
+    let boat_data = await axios.get(`${APIrouter}/get/boat-provider`)
     console.log('boat provider is',boat_data.data.payload);
     setBoatProviders(boat_data.data.payload)
   }
   const getTradition =async()=>{
-    let tradition_data = await axios.get('http://localhost:8080/get/traditions')
+    let tradition_data = await axios.get('${APIrouter}/get/traditions')
     console.log('tradition data is',tradition_data.data.payload);
     setKarenTraditions(tradition_data.data.payload.karen_tradition)
     setMonTraditions(tradition_data.data.payload.mon_tradition)
   }
   const getProduct=async()=>{
-    let product_data = await axios.get('http://localhost:8080/get/products')
+    let product_data = await axios.get('${APIrouter}/get/products')
     console.log('product is',product_data.data.payload);
     setProducts(product_data.data.payload)
   }
   const getRestaurant=async()=>{
-    let restaurant_data = await axios.get('http://localhost:8080/get/restaurant')
+    let restaurant_data = await axios.get('${APIrouter}/get/restaurant')
     console.log(restaurant_data.data.payload);
     let restaurant_payload = restaurant_data.data.payload
     if (restaurant_data.status === 200) {
@@ -167,7 +168,7 @@ export default function index() {
     }
   }
   const getReview=async()=>{
-    let review_data = await axios.get('http://localhost:8080/get/reviews')
+    let review_data = await axios.get('${APIrouter}/get/reviews')
     if(review_data.status === 200){
       let reviews = review_data.data.payload
       let set_review = []
