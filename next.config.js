f8ecd4f1e9490ebd41b47feb1cfe3c4c2898d398
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-18 23:12:44 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
   module.exports = {
     env: {
         DB_URL : "mongodb+srv://admin:1234@sangkhla.lm5wh.mongodb.net/Sangkhla2goDB",
-        // SERVER_API : "https://sangkhla2go-7uklpz68l-tuchy.vercel.app",
+        SERVER_API : "https://sangkhla2go-7uklpz68l-tuchy.vercel.app",
         // SERVER_API : "https://dee3-14-207-50-169.ngrok.io",
-        SERVER_API : "http://localhost:8080",
+        // SERVER_API : "http://localhost:8080",
         SERVER_IMAGE_PATH:'https://sangkhla-server.vercel.app/uploads/uploadImage',
         LOCAL_API : "http://localhost:8080" ,
         LOCAL_IMAGE_PATH : "http://localhost:8080/uploads/uploadImage",
