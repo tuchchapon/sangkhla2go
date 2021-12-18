91f6e5eeab2d1f0e271e452557a7860f1850660d
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-18 16:12:56 +0700

    new path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
     env: {
         DB_URL : "mongodb+srv://admin:1234@sangkhla.lm5wh.mongodb.net/Sangkhla2goDB",
         SERVER_API : "https://sangkhla2go-7uklpz68l-tuchy.vercel.app",
-        SERVER_IMAGE_PATH:'https://sangkhla-server.vercel.app/uploads/uploadimage',
+        SERVER_IMAGE_PATH:'https://sangkhla-server.vercel.app/uploads/uploadImage',
         LOCAL_API : "http://localhost:8080" ,
         LOCAL_IMAGE_PATH : "http://localhost:8080/uploads/uploadImage",
         FRONT_IMAGE_PATH : "http://localhost:8080/uploads"
