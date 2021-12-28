1506af211e6aa87c739b7629b3fba8b0f1fe4aed
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-28 20:55:54 +0700

    update attraction popup

--- components/attractionPopup.js
+++ components/attractionPopup.js
@@ -29,6 +29,7 @@ export default function attractionPopup({ activeAttraction, open, onClose }) {
 
     }
     return (
+
         <>
             <Popup
                 open={open}
@@ -40,21 +41,32 @@ export default function attractionPopup({ activeAttraction, open, onClose }) {
                 <div className={styles['popup-fixed-box']}>
                     <div className={styles['attraction-popup']}>
                         <img className={styles['popup-close-icon']} src='/Quit.png' onClick={() => onClose()} alt="" />
-                        <div className={styles['attraction-popup-namebox']} >
-                            <img src="/glass.png" alt="" />
-                            <span>{activeAttraction.name}</span>
-                        </div>
-                        <div className={styles['slider']}>
-                            <Slider {...settings} >
-                                {activeAttraction.images.length > 0 ? activeAttraction.images.map((image) => (
-                                    <div key={image} className={styles['slider-box']}  ><div className={styles['slider-image']} style={{ backgroundImage: `url(${image ? `${image}` : ''})` }}></div></div>
-                                )) : (
-                                    <div className={styles['slider-box']}  ><div className={styles['slider-image']} style={{ backgroundImage: `url('/no-image-big.png')` }}></div></div>
-                                )}
-                            </Slider>
-                        </div>
-                        <div className={styles['popup-detail-box']} >
-                            <span><b>คำบรรยาย : </b> {activeAttraction.detail ? activeAttraction.detail : ''}</span>
+                        <div className={styles['attraction-flexbox']}>
+
+                            <div className={styles['attraction-popup-namebox']} >
+                                <img src="/glass.png" alt="" />
+                                <span>{activeAttraction.name}</span>
+                            </div>
+                            <div className={styles['slider']}>
+                                <Slider {...settings} >
+                                    {activeAttraction.images.length > 0 ? activeAttraction.images.map((image) => (
+                                        <div key={image} className={styles['slider-box']}  ><div className={styles['slider-image']} style={{ backgroundImage: `url(${image ? `${image}` : ''})` }}></div></div>
+                                    )) : (
+                                        <div className={styles['slider-box']}  ><div className={styles['slider-image']} style={{ backgroundImage: `url('/no-image-big.png')` }}></div></div>
+                                    )}
+                                </Slider>
+                            </div>
+                            <div className={styles['popup-detail-box']} >
+                                <span><b>คำบรรยาย : </b> {activeAttraction.detail ? activeAttraction.detail : ''}</span>
+                            </div>
+                            <div className={styles['link-box']}>
+                                <div className={styles['link-1']}>
+                                    ข้อมูลเพิ่มเติม
+                                </div>
+                                <div className={styles['link-2']}>
+                                    ข้อมูลเพิ่มเติม
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
