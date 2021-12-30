7a892ff372b426272491d3ac6dd1e3e7c16a7e04
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-30 13:43:00 +0700

    Update forgotPassword.js

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {React,useState,useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import { useRouter } from "next/router";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -13,64 +13,62 @@ import Swal from 'sweetalert2';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-const forgotPassword =()=> {
+const forgotPassword = () => {
     const theme = createTheme();
-    const forgotPassword =()=>{
-        
+    const forgotPassword = () => {
+
         console.log('forgot password')
         console.log(userEmail);
         try {
-            axios.post(`${process.env.SERVER_API}/forgot-password`,userEmail).then((res)=>{
+            axios.post(`${process.env.SERVER_API}/forgot-password`, userEmail).then((res) => {
                 console.log(res);
-                if(res.status === 200){
-                    Swal.fire({
-                        title:`${res.data.payload}`,
-                        text:'กรุณาตรวจสอบอีเมลของท่าน',
-                        icon:'success'
-                    })
-                }
+                Swal.fire({
+                    title: `${res.data.payload}`,
+                    text: 'กรุณาตรวจสอบอีเมลของท่าน',
+                    icon: 'success'
+                })
             })
         } catch (error) {
             console.log(error);
         }
     }
-    const [userEmail, setUserEmail] = useState({email:'',token:''})
+    const [userEmail, setUserEmail] = useState({ email: '', token: '' })
     return (
         <div className="body">
             <ThemeProvider theme={theme} >
-                    <Container component="main" maxWidth="xs">
-                        <CssBaseline/>
-                            <Box  sx={{ marginTop: 8,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
-                                <Avatar>
-                                    S2
-                                </Avatar>
-                                <Typography component="form" variant="h5">
-                                    forgot password
-                                </Typography>
-                                <Box component="form" >
-                                    <TextField 
-                                    margin="normal"
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                   onChange={(e)=>{setUserEmail({...userEmail,email:e.target.value})} }/>
-                                   <Button
-                                   fullWidth
-                                   variant="contained"
-                                   sx={{mt:3,mb:2}}
-                                   onClick={forgotPassword}
-                                   >
-                                      เปลี่ยนรหัสผ่าน
-                                    </Button> 
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                        <Avatar>
+                            S2
+                        </Avatar>
+                        <Typography component="form" variant="h5">
+                            forgot password
+                        </Typography>
+                        <Box component="form" >
+                            <TextField
+                                margin="normal"
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={(e) => { setUserEmail({ ...userEmail, email: e.target.value }) }} />
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={forgotPassword}
+                            >
+                                เปลี่ยนรหัสผ่าน
+                            </Button>
 
-                                </Box>
-                            </Box>
-                    </Container>
-                </ThemeProvider>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </div>
     )
 }
-export default  forgotPassword
+export default forgotPassword
