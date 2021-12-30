d96991e7eebbbbcf38df260424af4b880a18fd7c
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2021-12-30 13:48:15 +0700

    Update forgotPassword.js

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -14,6 +14,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 const forgotPassword = () => {
+    const router = useRouter()
     const theme = createTheme();
     const forgotPassword = () => {
 
@@ -26,6 +27,11 @@ const forgotPassword = () => {
                     title: `${res.data.payload}`,
                     text: 'กรุณาตรวจสอบอีเมลของท่าน',
                     icon: 'success'
+
+                }).then((res) => {
+                    if (res.isConfirmed) {
+                        router.push('/login')
+                    }
                 })
             })
         } catch (error) {
