0a1691da535fcb0a21a36adfc0e09aceabd660f7
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2022-01-13 12:10:29 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -219,7 +219,7 @@ export default function index() {
         }
       },
       {
-        breakpoint: 400,
+        breakpoint: 700,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
