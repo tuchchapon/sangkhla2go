e272043f806b3a5609a24aa6ba857313879f2396
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2022-01-20 14:43:38 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,16 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import GoogleAnalytics from '../components/GoogleAnalytics';
 import ReactGa from 'react-ga'
 function MyApp({ Component, pageProps }) {
   const gaTrackingId = "G-XZ8Z3ZJX89"
   ReactGa.initialize(gaTrackingId)
   ReactGa.pageview("/")
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <GoogleAnalytics />
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -524,7 +524,7 @@ export default function index() {
             <title>Sangkhla2go</title>
 
             <Script
-              async src="https://www.googletagmanager.com/gtag/js?id=G-XZ8Z3ZJX89"
+              async src="https://www.googletagmanager.com/gtag/js?id=G-SB28B6FL04"
             />
             <Script
               dangerouslySetInnerHTML={{
@@ -532,7 +532,7 @@ export default function index() {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-XZ8Z3ZJX89');`
+                gtag('config', 'G-SB28B6FL04');`
               }}
             />
           </Head>
