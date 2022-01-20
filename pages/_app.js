58c7a6510e16dbf10727b511be9f88977ba2f829
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2022-01-20 15:23:19 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,6 @@ import "slick-carousel/slick/slick-theme.css";
 import GoogleAnalytics from '../components/GoogleAnalytics';
 import ReactGa from 'react-ga'
 function MyApp({ Component, pageProps }) {
-  const gaTrackingId = "G-XZ8Z3ZJX89"
-  ReactGa.initialize(gaTrackingId)
-  ReactGa.pageview("/")
   return <>
     <Component {...pageProps} />
     <GoogleAnalytics />
