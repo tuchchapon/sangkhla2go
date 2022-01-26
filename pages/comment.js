da9ca6dc183f6d6e27af9f46d49304a207f81874
Tuchchapon <35054396+tuchchapon@users.noreply.github.com>
2022-01-26 17:40:29 +0700

    update show more content button 3 size

--- pages/comment.js
+++ pages/comment.js
@@ -15,7 +15,7 @@ const validateEmail = (email) => {
         );
 };
 export default function comment() {
-
+    const [showComment, setShowComment] = useState([]);
     const [comments, setComments] = useState([]);
     const [showPopup, setShowPopup] = useState(false);
     const [checkData, setCheckData] = useState(false);
@@ -59,15 +59,71 @@ export default function comment() {
 
         console.log(newComment);
     }
-
+    const setNewshowComment = (new_show_arr) => {
+        setShowComment([...showComment, ...new_show_arr])
+    }
+    const showMore = (e) => {
+        if (e) e.preventDefault()
+        let new_show_arr = []
+        let new_res_comments = []
+        if (comments.length !== 0) {
+            if (screen.availWidth > 1400) {
+                for (let i = 0; i < 6; i++) {
+                    if (comments[i]) new_show_arr.push(comments[i])
+                }
+                new_res_comments = comments.splice(0, 6)
+            }
+            else if (screen.availWidth >= 768) {
+                for (let i = 0; i < 4; i++) {
+                    if (comments[i]) new_show_arr.push(comments[i])
+                }
+                new_res_comments = comments.splice(0, 4)
+            }
+            else if (screen.availWidth < 768) {
+                for (let i = 0; i < 3; i++) {
+                    if (comments[i]) new_show_arr.push(comments[i])
+                }
+                new_res_comments = comments.splice(0, 3)
+            }
+        }
+        setNewshowComment(new_show_arr)
+        console.log('new show arr is', new_show_arr);
+    }
     useEffect(() => {
 
         const getCommnet = async () => {
             const response = await axios.get(`${process.env.SERVER_API}/get/comment`)
-            console.log(response.data);
+            if (response.status === 200) {
+                console.log(response.data.payload.approve);
+                let data = response.data.payload.approve
+                // data
+                let setarr = []
+                if (screen.availWidth > 1400) {
+                    for (let j = 0; j < 6; j++) {
+                        if (data[j] === 0 || data[j]) setarr.push(data[j])
+                    }
+                    data.splice(0, 6)
+                }
+                else if (screen.availWidth >= 768) {
+                    for (let j = 0; j < 4; j++) {
+                        if (data[j] === 0 || data[j]) setarr.push(data[j])
+                        console.log('768');
+                    }
+                    data.splice(0, 4)
+                }
+                else if (screen.availWidth < 768) {
+                    for (let j = 0; j < 3; j++) {
+                        if (data[j] === 0 || data[j]) setarr.push(data[j])
+                    }
+                    data.splice(0, 3)
+                }
+                console.log('data is ', data);
+                console.log('setarr is', setarr);
+                setComments(data)
+                setShowComment(setarr)
+
+            }
             setCheckData(true)
-            setComments(response.data.payload.approve)
-            // setComments(response.data.payload.pending)
         }
         !checkData ? getCommnet() : null
 
@@ -109,13 +165,14 @@ export default function comment() {
                     <span>รีวิว</span>
                 </div>
                 <div className={styles['comment-list']}>
-                    {comments && comments.length > 0 ? comments.map((comment) => (
+                    {showComment && showComment.length > 0 ? showComment.map((comment) => (
                         <div key={comment.id} className={styles['comment-item']}>
                             <span className={styles['commentator-name']}>{comment.commentator_name ? comment.commentator_name : 'ไม่ระบุชื่อ'}</span>
                             <span className={styles['comment-text']}>{comment.comment_text}</span>
                         </div>
                     )) : null}
                 </div>
+                {comments.length > 0 ? <span onClick={(e) => showMore(e)} className={styles['load-content-text']}>แสดงเพิ่ม</span> : null}
             </div>
             <CommentPopup open={showPopup} onClose={() => setShowPopup(false)} />
         </div>
